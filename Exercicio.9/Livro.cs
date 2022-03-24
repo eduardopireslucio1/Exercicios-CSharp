d297ce543e7d5bf50ecd35eff523850c8775c08a
eduardopireslucio1 <eduardopireslucioo@gmail.com>
2022-03-23 22:35:18 -0300

    Exercicios aula 23-03

---
+++ Exercicio.9/Livro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio._9
+{
+    internal struct Livro
+    {
+        public string Titulo { get; private set; }
+        public string Autor { get; private set; }
+        public string Valor { get; private set; }
+
+        public Livro(string titulo, string autor, string valor)
+        {
+            Titulo = titulo ?? throw new ArgumentNullException(nameof(titulo);
+            Autor = autor ?? throw new ArgumentNullException(nameof(autor);
+            Valor = valor;
+        }
+
+        public override string ToString() 
+            => $"Autor: {Autor} \n Titulo: {Titulo} \n Valor: {Valor}";
+    }
+}
---
+++ Exercicio.9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercicio._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Livro("Teste", "Teste", 99.90m).ToString());
+            Console.ReadLine();
+        }
+    }
+}
