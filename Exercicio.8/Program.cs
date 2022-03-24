31dba691dece99290b32bb455f781565dd64f5d2
eduardopireslucio1 <eduardopireslucioo@gmail.com>
2022-03-23 22:12:29 -0300

    Aula 23-03

---
+++ Exercicio.8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercicio.oito
+{
+    class Program
+    {
+        private static readonly char[] _vogais = new char[]
+        {
+            'a', 'e', 'i', 'o', 'u',
+        };
+
+        //private const char _a = 'a';
+        //private const char _e = 'e';
+        //private const char _i = 'i';
+        //private const char _o = 'o';
+        //private const char _u = 'u';
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite seu nome:");
+                var nome = Console.ReadLine().ToLower();
+                var count = 0;
+
+                foreach(var letra in nome)
+                {
+                    if (!_vogais.Contains(letra))
+                        count++;
+                }
+
+                //for(int i = 0; i <= nome.Length; i++)
+                //{
+                   // var letra = nome[i];
+
+                    //if (!_a.Equals(letra)
+                    //    &&!_e.Equals(letra) &&
+                    //    count++;
+                //}
+
+                Console.Write($"Seu nome possui {count}" +
+                    $" consoantes!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exercicio.sete/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercicio.sete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Informe seu nome:");
+                var nome = Console.ReadLine();
+
+                Console.WriteLine("Informe sua data de nascimento no formato dd/MM/yyyy:");
+                //Passando por referência
+                //DateTime.TryParse(Console.ReadLine(), out DateTime dataNascimento);
+                var dataNascimento = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine(ObterMensagemFaixaEtaria(nome, dataNascimento));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static string ObterMensagemFaixaEtaria(string nome, DateTime dataNascimento)
+        {
+            var idade = DateTime.Today.Year - dataNascimento.Year;
+
+            if (idade >= 60)
+                return $"Olá {nome}, você está na faixa etária idoso!";
+            else if (idade >= 20 && idade <= 79)
+                return $"Olá {nome}, você está na faixa etária Adulto!";
+            return $"Olá {nome}, você está na faixa etária Jovem!";
+
+
+        }
+    }
+}
